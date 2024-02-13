516e047b5905127d0ec009b3b79c220f61260a33
CHRISTIAN AVELINO-MILLER <AVELICHR000@student.evergreenps.org>
2024-02-13 10:45:13 -0800

    wqweqweq
    qweqweqweq

--- S2.1.0/src/Account.java
+++ S2.1.0/src/Account.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Account
 {
     private String accountName;
@@ -5,22 +7,31 @@ public class Account
     private String phoneNumber;
     private String creationDate;
     private int accountAge;
+    private int[] dice=new int[5];
     
-    public Account(String accountName, String email, String phoneNumber, String creationDate, int accountAge)
+    public Account(String accountName,String email,String phoneNumber,String creationDate,int accountAge)
     {
-    	this.accountName = accountName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.creationDate = creationDate;
-        this.accountAge = accountAge;
+    	this.accountName=accountName;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+        this.creationDate=creationDate;
+        this.accountAge=accountAge;
     }
 
     public void print()
     {
-        System.out.println("Name: " + accountName);
-        System.out.println("Email: " + email);
-        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Name: "+accountName);
+        System.out.println("Email: "+email);
+        System.out.println("Phone Number: "+phoneNumber);
         System.out.println("Account Creation Date: "+creationDate);
         System.out.println("Current Account Age(Years): "+accountAge);
+    }
+    
+    public void roll(boolean j[])
+    {
+    	Random rand = new Random();
+    	for(int i=0;i<dice.length;i++)
+    		dice[i]=j[i]?rand.nextInt(6)+1:dice[i];
+    	System.out.println(Arrays.toString(dice));
     }
 }
--- S2.1.0/src/Main.java
+++ S2.1.0/src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 public class Main
 {
 	public static void main(String[] args)
     {
+		boolean[] lean = new boolean[6];
 		Account a1 = new Account("Jim", "12345@yahoo.com", "503-1234-567", "2/12/2022", 2);
 		a1.print();
+		System.out.println(Arrays.toString(lean));
+		a1.roll(lean);
     }
 }
