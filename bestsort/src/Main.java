28945ba6aacd9d7b6915ed3c94a04e22f66d2dbe
CHRISTIAN AVELINO-MILLER <AVELICHR000@student.evergreenps.org>
2024-03-14 10:46:36 -0700

    sdfdf
    sdfsdf

--- bestsort/src/Main.java
+++ bestsort/src/Main.java
@@ -1,15 +1,16 @@
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
-
+/*GAME PLAN: make classes that standardize file types to a main file
+ * so that this algorithm can read and sort them for interpretation.
+ * also make this one recursive so it can sort larger amounts of data faster.
+ */
 public class Main
 {
   public static void main(String[] args)
   {
 	  Random rand=new Random();
-	  int n=100000,saver=0,Ssvr;
-	  long changes=0;
-	  boolean[] newH=new boolean[n];
+	  int n=1000000,saver=0;
 	  boolean saved=false;
 	  int[] dset=new int[n];
 	  for(int i=0;i<n;i++)
@@ -17,10 +18,12 @@ public class Main
 	  long start=System.nanoTime();
 	  for(int i=0;i<n-1;)
 	  {
-		  if(!newH[i])
+		  if(i>100)
 		  {
-			  Ssvr=dset[i];
-			  newH[i]=true;
+			  if(dset[i]>dset[100])
+			  {
+				  dset[i]=(dset[i]+dset[100])-(dset[100]=dset[i]);
+			  }
 		  }
 		  if(dset[i]>dset[i+1])
 		  {
@@ -29,10 +32,8 @@ public class Main
 				  saver=i;
 				  saved=true;
 			  }
-			  dset[i] = (dset[i] + dset[i+1]) - (dset[i+1] = dset[i]);
-			  if(i==0) i=0;
-			  else i--;
-			  changes++;
+			  dset[i]=(dset[i]+dset[i+1])-(dset[i+1]=dset[i]);
+			  i=(i==0)?(0):(i-1);
 		  }
 		  else
 		  {
@@ -48,6 +49,5 @@ public class Main
 	  long timeInMillis=TimeUnit.MILLISECONDS.convert(end-start,TimeUnit.NANOSECONDS);
 	  System.out.println(Arrays.toString(dset));
 	  System.out.println("Time spend in ms: "+timeInMillis);
-	  System.out.println(changes);
   }
 }
